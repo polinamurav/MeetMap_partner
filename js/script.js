ad7511bf503f6b661995c973c46394c03609dc3d
Polina Muravieva <polinamur3@gmail.com>
2024-12-14 16:31:22 +0300

    added popup partner

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ $('.carousel-slide').slick({
     slidesToScroll: 3,
     dots: true,
 });
+
+//карточки партнеров
+const partner = document.getElementsByClassName('partner-item');
+for (let i = 0; i < partner.length; i++) {
+    partner[i].onclick = function () {
+        let popup = partner[i].nextElementSibling;
+        if (popup && popup.classList.contains('partner-item-popUp')) {
+            popup.style.display = 'flex';
+        }
+    }
+}
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('popup-close')) {
+        e.target.closest('.partner-item-popUp').style.display = 'none';
+    }
+});
